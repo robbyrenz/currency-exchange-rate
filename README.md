190ae11fbb8217e085dc6845c221dff35e8341ea
Robby Renz <nxgrenz@gmail.com>
2019-10-02 23:16:52 -0500

    web app complete, displays correct exchange rate

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Foreign Currency Exchange Rate
-A website that was built with Flask in the backend that lets the user find out the most recent exchange rate for the currencies GBP (Great British Pound), AUD (Austrailian Dollar), CAD (Canadian Dollar), PLN (Polish Zioty), and MXN (Mexican Peso) from EUR (Euro). This Python program uses the Fixer API in order to get the latest conversion rates.
+A website that was built with Flask in the backend that lets the user find out the most recent exchange rate for the currencies GBP (Great British Pound), AUD (Austrailian Dollar), CAD (Canadian Dollar), PLN (Polish Zioty), and MXN (Mexican Peso) from 1 EUR (Euro). This Python program uses the Fixer API in order to get the latest foreign conversion rates.
--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from flask import Flask, render_template, request
 
 # grab the API key from the environment variable
@@ -9,8 +10,25 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template("index.html")
+    if request.method == 'GET':  # user did not send anything
+        return render_template("index.html")  # render the default homepage
+
+    currency = request.form.get("currency")
+    rate = get_rate(currency)
+    return render_template("index.html", rate=rate, currency=currency)
+
+
+def get_rate(currency):
+    """
+    gets the curent exchange rate of the given currency.
+    :param currency: the currency to be checked against 1 EUR.
+    :return: the rate of the currency relative to 1 EUR.
+    """
+    currency = currency.upper()
+    r = requests.get(f"http://data.fixer.io/api/latest?access_key={API_KEY}&symbols={currency}")
+    r = r.json()
+    rate = r["rates"][currency]
+    return rate
 
 
 if __name__ == "__main__":
